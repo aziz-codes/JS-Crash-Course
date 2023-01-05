be661947d871547594e4e38d5a04bbf66b5ce369
aziz-codes <azizisoffline@gmail.com>
2023-01-05 18:51:36 +0500

    string vs numbers in js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+"strict"
 let count = 0;
+let countEl = document.getElementById('counter');
 
 function increment(){
     count+=1;
-    document.getElementById('counter').innerText = count;;
+    countEl.innerText = count;;
 }
+
+
+function save(){
+    console.log(count);
+}
+
+  let name = "Aziz";
+  let greeting = "This is my greeting message";
+
+  let myGreeting = name+" "+greeting;
+  console.log(myGreeting)
