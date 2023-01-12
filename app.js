d0320e014e449176d38ba5852b89df7b58ecf2ba
aziz-codes <azizisoffline@gmail.com>
2023-01-12 23:12:44 +0500

    building blackjack game

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-let errorEl = document.getElementById('error-message');
+let num1 = 5;
+let num2 = 7;
+let message = "";
+let sum = num1+num2;
+const cardsEl = document.getElementById('card-el');
+const sumEl = document.getElementById('sum-el');
+const messageEl = document.getElementById('message-el');
+function startGame(){
+cardsEl.textContent = "Cards: "+num1+ " "+num2;
+  sumEl.textContent = "Sum: "+sum;
 
-function handlePurchase(){
-    errorEl.textContent = "Something went wrong, please try again";
+  if(sum<=20){
+    message = "Do you want to draw a new card ? ";
+  }
+  else if(sum===21){
+    message = "Boom , you've got a black jack";
+  }
+  else
+  message = "You are out the game";
+
+  messageEl.textContent = message;
+}
+
+function newCard()
+{
+   let num3 = 7;
+   sum+=num3;
+   startGame();
 }
