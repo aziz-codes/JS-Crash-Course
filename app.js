97e005ebafe774b8aacc8938632fef95fc6a3c8e
aziz-codes <azizisoffline@gmail.com>
2023-01-13 00:15:19 +0500

    3 draw limits cards

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-
-let count = 0;
-let countEl = document.getElementById('counter');
-let entriesEl = document.getElementById('entries-el');
-let entries = "Previous Entires : ";
-
-
-function increment(){
-    count+=1;
-    countEl.innerText = count;;
-    
+let counts = 3;
+let sum = 0;
+const cardsEl = document.getElementById('cards-el');
+let cards = 0;
+let isAlive = true;
+function replay(){
+    counts = 4;
+    sum = 0;
+    isAlive = true;
 }
-
-function save(){
-    entries+=count+" - ";
-    entriesEl.textContent = entries;
-}
-
-function Parent(){
-   let myCount = 0;
-
-    function Child(){
-   myCount = 6;
-   console.log(myCount);
+function drawCard(){
+counts-=1;
+if(counts>=0){
+   cards = Math.floor(Math.random()*11)+1;
+   cardsEl.textContent+=cards+"      ";
+   sum+=cards;
+   document.getElementById('remaing-card-el').textContent = counts;
+    document.getElementById('sum-el').textContent=sum;
+   if(sum<=20){
+    document.getElementById('message-el').textContent = "Do you want to draw again";
+    if(counts===0){
+        document.getElementById('message-el').textContent = "Your limit has reached";
+        document.getElementById('replay-btn').style.display="block";
+    }
    }
-  return Child();
+   else if(sum===21){
+    document.getElementById('message-el').textContent = "Black Jack";
+    document.getElementById('replay-btn').style.display="block";
+   }
+   else{
+    document.getElementById('message-el').textContent = "You are out of Game";
+     isAlive = false;
+     document.getElementById('replay-btn').style.display="block";
+
+   }
 }
 
-
-Parent();
+}
