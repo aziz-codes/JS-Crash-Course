7213d15a21d0320c369c5ce50f8d2a26025a9f80
aziz-codes <azizisoffline@gmail.com>
2023-01-12 23:47:17 +0500

    done with basic blackjack game

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-let num1 = 5;
-let num2 = 7;
+let num1 = Math.floor(Math.random()*10)+1;
+let num2 = Math.floor(Math.random()*10)+1;
+let cards = [];
+let isAlive = true;
+let hasBlackjack = false;
+cards.push(num1,num2);
 let message = "";
 let sum = num1+num2;
 const cardsEl = document.getElementById('card-el');
 const sumEl = document.getElementById('sum-el');
 const messageEl = document.getElementById('message-el');
+
 function startGame(){
-cardsEl.textContent = "Cards: "+num1+ " "+num2;
+    cardsEl.textContent ="";
+    for(let i = 0; i<cards.length; i++){
+        cardsEl.textContent += cards[i]+" ";
+    }
   sumEl.textContent = "Sum: "+sum;
 
   if(sum<=20){
@@ -15,15 +23,20 @@ cardsEl.textContent = "Cards: "+num1+ " "+num2;
   else if(sum===21){
     message = "Boom , you've got a black jack";
   }
-  else
-  message = "You are out the game";
-
+  else{
+    message = "You are out the game";
+    hasBlackjack = false;
+    isAlive = false;
+  }
   messageEl.textContent = message;
 }
 
 function newCard()
 {
-   let num3 = 7;
+   if(isAlive ===true && hasBlackjack===false){
+    let num3 = Math.floor(Math.random()*20)+1;
    sum+=num3;
+   cards.push(num3);
    startGame();
+   }
 }
