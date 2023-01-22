45a35c16e2e9cc5b8af8e79055c9b6c6ba17c705
aziz-codes <azizisoffline@gmail.com>
2023-01-23 00:59:17 +0500

    time stamp 5:447, creating menifest.json file

--- extension/app.js
+++ extension/app.js
@@ -4,12 +4,15 @@ const inputEl = document.getElementById("input-el");
 const lists = document.getElementById("lists");
 inputBtn.addEventListener("click", () => {
   let link = inputEl.value;
-  if (myLeads.includes(link)) {
-    alert("sorry this path already exists");
-  } else {
-    myLeads.push(link);
-  }
-  for (let i = 0; i < myLeads.length; i++) {
-    lists.textContent += myLeads[i];
-  }
+  myLeads.push(link);
+  renderLeads();
+  inputEl.value = "";
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a target='_blank' href=${myLeads[i]}>${myLeads[i]}</a></li>`;
+  }
+  lists.innerHTML = listItems;
+}
