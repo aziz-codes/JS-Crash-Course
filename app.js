056a5226cf6c0b4b5e627ca27bb2834c83a9243f
aziz-codes <azizisoffline@gmail.com>
2023-01-22 21:37:14 +0500

    generating random item from array

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-let num1 = Math.floor(Math.random()*10)+1;
-let num2 = Math.floor(Math.random()*10)+1;
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
 let cards = [];
 let isAlive = true;
 let hasBlackjack = false;
-cards.push(num1,num2);
+cards.push(num1, num2);
 let message = "";
-let sum = num1+num2;
-const cardsEl = document.getElementById('card-el');
-const sumEl = document.getElementById('sum-el');
-const messageEl = document.getElementById('message-el');
+let sum = num1 + num2;
+const cardsEl = document.getElementById("card-el");
+const sumEl = document.getElementById("sum-el");
+const messageEl = document.getElementById("message-el");
 
-function startGame(){
-    cardsEl.textContent ="";
-    for(let i = 0; i<cards.length; i++){
-        cardsEl.textContent += cards[i]+" ";
-    }
-  sumEl.textContent = "Sum: "+sum;
+function startGame() {
+  cardsEl.textContent = "";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
 
-  if(sum<=20){
+  if (sum <= 20) {
     message = "Do you want to draw a new card ? ";
-  }
-  else if(sum===21){
+  } else if (sum === 21) {
     message = "Boom , you've got a black jack";
-  }
-  else{
+  } else {
     message = "You are out the game";
     hasBlackjack = false;
     isAlive = false;
@@ -31,33 +29,19 @@ function startGame(){
   messageEl.textContent = message;
 }
 
-function newCard()
-{
-   if(isAlive ===true && hasBlackjack===false){
-    let num3 = Math.floor(Math.random()*20)+1;
-   sum+=num3;
-   cards.push(num3);
-   startGame();
-   }
+function newCard() {
+  if (isAlive === true && hasBlackjack === false) {
+    let num3 = Math.floor(Math.random() * 20) + 1;
+    sum += num3;
+    cards.push(num3);
+    startGame();
+  }
 }
 
-// recap and assesment at timestamp : 04:16:25
+let array = ["Rock", "Paper", "Scissor"];
 
- let age = 90;
- let discount = "";
- if(age < 6){
-  discount = "Free";
- }
- else if(age > 6 && age < 17){
-    discount = "Child Discount";
- }
- else if(age > 18 && age < 26){
-    discount = "Student discount";
- }
- else if(age > 27 && age < 66){
-    discount = "Full Price";
- }
- else {
-    discount = "Senior Citizen Discount";
- }
- console.log(discount);
+function randomItem() {
+  let index = Math.floor(Math.random() * 3) + 0;
+  console.log(array[index]);
+}
+randomItem();
