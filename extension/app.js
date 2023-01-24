71262f3656320287a9e2a2898749b914c2226f6b
aziz-codes <azizisoffline@gmail.com>
2023-01-25 00:41:54 +0500

    fetching items from localstorage and delete them

--- extension/app.js
+++ extension/app.js
@@ -1,17 +1,27 @@
 const inputBtn = document.getElementById("input-btn");
-let myLeads = ["www.github.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const lists = document.getElementById("lists");
+const deleteBtn = document.getElementById("delete-btn");
+const leads = JSON.parse(localStorage.getItem("myLeads"));
+if (leads) {
+  myLeads = leads;
+  renderLeads();
+}
+//handler to add item to the localstorage.
 inputBtn.addEventListener("click", () => {
   let link = inputEl.value;
   myLeads.push(link);
   inputEl.value = "";
-
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
   renderLeads();
 });
-
+// handler to remove all the links from localstorage and clean up the DOM.
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
