7a3ea2363167d61a2118a830ee93cfc476bd5a3f
aziz-codes <azizisoffline@gmail.com>
2023-01-25 00:51:31 +0500

    refactoring js functions

--- extension/app.js
+++ extension/app.js
@@ -1,12 +1,14 @@
+//Dom Elements
 const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const lists = document.getElementById("lists");
 const deleteBtn = document.getElementById("delete-btn");
-const leads = JSON.parse(localStorage.getItem("myLeads"));
-if (leads) {
-  myLeads = leads;
-  renderLeads();
+
+const leadsInLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsInLocalStorage) {
+  myLeads = leadsInLocalStorage;
+  render(myLeads);
 }
 //handler to add item to the localstorage.
 inputBtn.addEventListener("click", () => {
@@ -14,18 +16,18 @@ inputBtn.addEventListener("click", () => {
   myLeads.push(link);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
 });
 // handler to remove all the links from localstorage and clean up the DOM.
 deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a target='_blank' href=${myLeads[i]}>${myLeads[i]}</a></li>`;
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li><a target='_blank' href=${leads[i]}>${leads[i]}</a></li>`;
   }
   lists.innerHTML = listItems;
 }
